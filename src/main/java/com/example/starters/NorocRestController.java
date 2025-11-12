a120744800ed45719cb35f6e18a9de29fbab6344
CristianM <mija.cristian3101@gmail.com>
2025-11-12 20:06:02 +0200

    minor changes

---
+++ src/main/java/com/example/starters/NorocRestController.java
@@ -0,0 +1,13 @@
+package com.example.starters;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NorocRestController {
+
+    @GetMapping
+    public String mesaj() {
+        return "Hai noro9hhh9h9hgt08h9j[iopbvfckvljkhlj;lgkhfoulgnikh8iyhp9oi5c!";
+    }
+}
